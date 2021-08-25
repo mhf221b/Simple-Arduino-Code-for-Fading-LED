5f74e779ba41c3946a7956d9f2e055392f1dc3ff
Mehedi Hasan <mhf221b@gmail.com>
2021-08-25 22:33:07 +0600

    Uploading a Code

---
+++ sketch_aug25a.ino
@@ -0,0 +1,22 @@
+int ledPin = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(ledPin, OUTPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int fadeValue =0; fadeValue<=255; fadeValue+=5){
+    analogWrite(ledPin, fadeValue);
+    delay(30);
+  }
+  for (int fadeValue =255; fadeValue>=0; fadeValue-=5){
+    analogWrite(ledPin, fadeValue);
+    delay(30);
+  }
+
+}
